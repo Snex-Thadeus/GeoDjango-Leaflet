2988afe4d1a1bd9cec1dde2e15f1371e7a4a21c7
Snex-Thadeus <lwande94@gmail.com>
2022-10-11 17:42:36 +0300

    Created new app Note

--- coffe_shop_mapper/settings.py
+++ coffe_shop_mapper/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     'resources',
     'leaflet',
+    'note',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +93,7 @@ DATABASES = {
         'NAME': 'coffeMapper',
         'USER':'postgres',
         'HOST': 'localhost',
-        'PASSWORD': '****',
+        'PASSWORD': '',
         'PORT': '5432',
     }
 }
@@ -133,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- coffe_shop_mapper/urls.py
+++ coffe_shop_mapper/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from note.views import note
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resources.urls')),
+    path('note/', note, name='note'),
 ]
